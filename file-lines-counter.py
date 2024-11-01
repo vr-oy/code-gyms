a86cac79d53273c596e83674b499c5281778dece
Anah Victor <30688175+anahvictoronyedic@users.noreply.github.com>
2024-11-01 13:38:38 +0100

    Feat: add more ways to count while also using an assertion to verify

--- file-lines-counter.py
+++ file-lines-counter.py
@@ -5,6 +5,12 @@ def file_len(fname):
             pass
     return i + 1
 
+# first solution
 num_of_lines = file_len("my_file.txt")
 
 print(num_of_lines)
+
+# second solution
+num_of_lines2 = sum(1 for l in open('my_file.txt'))
+
+assert num_of_lines is num_of_lines2
