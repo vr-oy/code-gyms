15c279f4c548ad068f64ef43cc6a16c7d1bfccb7
Anah Victor <30688175+anahvictoronyedic@users.noreply.github.com>
2024-11-16 22:21:40 +0100

    refactor: organize the code documentation

--- efficient-num-prime-factors-printer.py
+++ efficient-num-prime-factors-printer.py
@@ -2,8 +2,7 @@
 
 import math
 
-# A function to print all prime factors of
-# a given number n
+# A function to print all prime factors of a given number n
 def primeFactors(n):
 	
 	# Print the number of two's that divide n
@@ -15,13 +14,12 @@ def primeFactors(n):
 	# so a skip of 2 ( i = i + 2) can be used
 	for i in range(3,int(math.sqrt(n))+1,2):
 		
-		# while i divides n , print i ad divide n
+		# while i divides n, print i ad divide n
 		while n % i== 0:
 			print(i)
 			n = n // i
 			
-	# Condition if n is a prime
-	# number greater than 2
+	# Condition if n is a prime number greater than 2
 	if n > 2:
 		print(n)
 		
