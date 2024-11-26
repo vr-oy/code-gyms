5b36effadac7664148c04efebdea32b374a14d7d
Anah Victor <30688175+anahvictoronyedic@users.noreply.github.com>
2024-11-26 23:54:31 +0100

    feat: add code to print 3 column calendar for a year

---
+++ three-column-calendar-printer.py
@@ -0,0 +1,15 @@
+# Import the calendar module
+import calendar
+# Create a TextCalendar object starting from Sunday as the first day of the week
+cal = calendar.TextCalendar(calendar.SUNDAY)
+
+year = 2022
+
+# Specify the formatting parameters for the year calendar
+# year
+# column width: 2
+# lines per week: 1
+# number of spaces between month columns: 1
+# 3: number of months per column
+# Generate the formatted year calendar for the year using the specified parameters
+print(cal.formatyear(year, 2, 1, 1, 3))
