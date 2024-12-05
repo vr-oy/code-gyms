fb991603e5086c7a67a0251b34891ba4eab1d75a
Anah Victor <30688175+anahvictoronyedic@users.noreply.github.com>
2024-12-05 22:32:46 +0100

    refactor: organize module import

--- yesterday-today-tomorrow-dates-printer.py
+++ yesterday-today-tomorrow-dates-printer.py
@@ -1,14 +1,13 @@
-# Import the datetime module
-import datetime 
+from datetime import date, timedelta
 
 # Get the current date using datetime.date.today()
-today = datetime.date.today()
+today = date.today()
 
 # Calculate yesterday's date by subtracting 1 day from today's date using timedelta(days=1)
-yesterday = today - datetime.timedelta(days=1)
+yesterday = today - timedelta(days=1)
 
 # Calculate tomorrow's date by adding 1 day to today's date using timedelta(days=1)
-tomorrow = today + datetime.timedelta(days=1) 
+tomorrow = today + timedelta(days=1) 
 
 # Print yesterday's date
 print('Yesterday : ', yesterday)
