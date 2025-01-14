891f50aa17420d2c9ab2de8b93edab05b39f5f10
Anah Victor <30688175+anahvictoronyedic@users.noreply.github.com>
2025-01-14 23:06:51 +0100

    refactor: get pattern character from user

--- right-angle-triangle-pattern-printer.py
+++ right-angle-triangle-pattern-printer.py
@@ -1,8 +1,9 @@
-def myfunc(n):
+def myfunc(ch,n):
 	for i in range(0, n):
 		for j in range(0, i+1):
-			print("* ",end="")
+			print("%s " % ch,end="")
 		print("\r")
 
+ch = input('Enter the pattern character:') or '*'
 n = int(input('Enter the pattern size:'))
-myfunc(n)
+myfunc(ch,n)
