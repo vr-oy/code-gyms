9266e871651248e30d664cf74ddef7b962eaaaab
Anah Victor Onyedikachi <anahvictoronyedic@gmail.com>
2025-02-26 23:53:15 +0100

    feat: add util to get number list from user

---
+++ py/utils/input_utils.py
@@ -0,0 +1,24 @@
+from typing import Never
+
+class Input_Utils:
+    @classmethod
+    def get_number_list_interactively(cls) -> list[int] | None:
+        print('Enter the comma separated numbers:')
+        try:
+            nums = cls.get_number_list()
+            print("List of numbers:", nums)
+            return nums
+        except ValueError as e:
+            print('the input is invalid')
+            return None
+    
+    @staticmethod
+    def get_number_list() -> list[int] | Never:
+        list_str:str = input()
+        if(not list_str or not all(map(lambda item : item.isnumeric(),list_str.split(',')))):
+            raise ValueError('Invalid input')
+
+        # Create a list 'nums' with specific elements
+        nums = list(map(int,list_str.split(',')))
+
+        return nums
